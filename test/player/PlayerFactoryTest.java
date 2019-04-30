6d2035e6a36ed2175a885a35f03adbd85249b6d3
Tal, Shahar (st9077) <st9077@intl.att.com>
2019-04-30 10:10:55 +0300

    Refactored PlayerFactoryTest to use one test and added parameters

--- test/player/PlayerFactoryTest.java
+++ test/player/PlayerFactoryTest.java
@@ -67,7 +67,6 @@ public class PlayerFactoryTest {
                 return;
             case PLAYER_BOOKMARK_EACH_STEP:
                 Assert.assertTrue(player instanceof PlayerBookmarkEachStep);
-                return;
         }
     }
 }
