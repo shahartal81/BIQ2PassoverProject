626ca740c4c6a33c2f8bea14f9fcdd02bef12c7a
Tal, Shahar (st9077) <st9077@intl.att.com>
2019-04-30 10:09:17 +0300

    Refactored PlayerFactoryTest to use one test and added parameters

--- test/player/PlayerFactoryTest.java
+++ test/player/PlayerFactoryTest.java
@@ -4,15 +4,47 @@ import additionalclasses.Position;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.mockito.Mockito.when;
 
+@RunWith(Parameterized.class)
 public class PlayerFactoryTest {
 
+    private enum PlayerTypes {
+        PLAYER_SIMPLE,
+        PLAYER_VERY_ADVACNED,
+        PLAYER_BOOKMARK_EACH_STEP
+    }
+
     private PlayerFactory playerFactory;
+    private int maxSteps;
+    private int rows;
+    private int cols;
+    private PlayerTypes playerInd;
+
+    @Parameterized.Parameters(name = "Test #{index} - Check if Max Steps = {0}, Rows = {1} Columns = {2} then Player = {3}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {30, 3, 3, PlayerTypes.PLAYER_SIMPLE},
+                {30, 10, 10, PlayerTypes.PLAYER_VERY_ADVACNED},
+                {3000, 15, 15, PlayerTypes.PLAYER_BOOKMARK_EACH_STEP}
+        });
+    }
+
+    public PlayerFactoryTest(int m, int r, int c, PlayerTypes ind) {
+        this.maxSteps = m;
+        this.rows = r;
+        this.cols = c;
+        this.playerInd = ind;
+    }
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -21,33 +53,21 @@ public class PlayerFactoryTest {
     private Position position;
 
     @Test
-    public void testPlayerSimple(){
-        when(position.getColumn()).thenReturn(3);
-        when(position.getRow()).thenReturn(3);
-        int maxSteps = 30;
+    public void testPlayerFactory(){
+        when(position.getColumn()).thenReturn(cols);
+        when(position.getRow()).thenReturn(rows);
         playerFactory = new PlayerFactory();
         Player player = playerFactory.createPlayer(new Position(position.getRow(), position.getColumn()), maxSteps);
-        Assert.assertTrue(player instanceof PlayerSimple);
+        switch (playerInd){
+            case PLAYER_SIMPLE:
+                Assert.assertTrue(player instanceof PlayerSimple);
+                return;
+            case PLAYER_VERY_ADVACNED:
+                Assert.assertTrue(player instanceof PlayerVeryAdvanced);
+                return;
+            case PLAYER_BOOKMARK_EACH_STEP:
+                Assert.assertTrue(player instanceof PlayerBookmarkEachStep);
+                return;
+        }
     }
-
-    @Test
-    public void testPlayerVeryAdvanced(){
-        when(position.getColumn()).thenReturn(10);
-        when(position.getRow()).thenReturn(10);
-        int maxSteps = 30;
-        playerFactory = new PlayerFactory();
-        Player player = playerFactory.createPlayer(new Position(position.getRow(), position.getColumn()), maxSteps);
-        Assert.assertTrue(player instanceof PlayerVeryAdvanced);
-    }
-
-    @Test
-    public void testPlayerBookmarkEachStep(){
-        when(position.getColumn()).thenReturn(10);
-        when(position.getRow()).thenReturn(10);
-        int maxSteps = 3000;
-        playerFactory = new PlayerFactory();
-        Player player = playerFactory.createPlayer(new Position(position.getRow(), position.getColumn()), maxSteps);
-        Assert.assertTrue(player instanceof PlayerBookmarkEachStep);
-    }
-
 }
