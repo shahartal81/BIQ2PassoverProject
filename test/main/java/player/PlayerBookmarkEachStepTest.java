29dd089d2958c378e0107a4e5e3558530543eb93
Tal, Shahar (st9077) <st9077@intl.att.com>
2019-05-01 11:02:08 +0300

    Added unit tests for PlayerBookmarkEachStep

---
+++ test/main/java/player/PlayerBookmarkEachStepTest.java
@@ -0,0 +1,45 @@
+package main.java.player;
+
+import main.java.enums.Move;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerBookmarkEachStepTest {
+
+    @Test
+    public void testPlayerBookmarkEachStepMoveMethod(){
+        PlayerBookmarkEachStep player = new PlayerBookmarkEachStep();
+        Move move = player.move();
+        Assert.assertTrue(move.equals(Move.DOWN) || move.equals(Move.UP) || move.equals(Move.LEFT) || move.equals(Move.RIGHT));
+        Assert.assertTrue("next turn bookmark flag should be true", player.isNextTurnBookmark());
+        move = player.move();
+        Assert.assertEquals("move should be bookmark", Move.BOOKMARK, move);
+    }
+
+    @Test
+    public void testPlayerBookmarkEachStepHitWallMethod(){
+        PlayerBookmarkEachStep player = new PlayerBookmarkEachStep();
+        Move move = player.move();
+        Assert.assertEquals("move is not saved as last move", move, player.getLastMove());
+        Assert.assertEquals("bookmark sequence should be 0", 0, player.getSeqNumber());
+        player.hitWall();
+        Assert.assertEquals("bookmark sequence should be 1", 1, player.getSeqNumber());
+        Assert.assertTrue("bookmark 1 should be in map", player.getBookmarksMap().containsKey(1));
+        Assert.assertEquals("bookmark 1 should contain last move as value", move, player.getBookmarksMap().get(1).get(0));
+    }
+
+    @Test
+    public void testPlayerBookmarkEachStepHitBookmarkMethod(){
+        PlayerBookmarkEachStep player = new PlayerBookmarkEachStep();
+        player.move();
+        player.hitWall();
+        player.move();
+        player.hitBookmark(1);
+        Assert.assertEquals("hit bookmark sequence should be 1", 1, player.getHitBookmarkSeqNumber());
+        Move move = player.move();
+        player.hitBookmark(2);
+        Assert.assertEquals("hit bookmark sequence should be 2", 2, player.getHitBookmarkSeqNumber());
+        Assert.assertTrue("bookmark 2 should be in map", player.getBookmarksMap().containsKey(2));
+        Assert.assertEquals("bookmark 2 should contain last move as value", move, player.getBookmarksMap().get(2).get(0));
+    }
+}
