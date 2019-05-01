e78d6c72939309170e60d6d62a0d51ed48d39743
Yuval Lev Naor <yl0871@intl.att.com>
2019-05-01 13:37:38 +0300

    Added basic unit tests for GameManager class

--- test/main/java/gamemanager/GameManagerTest.java
+++ test/main/java/gamemanager/GameManagerTest.java
@@ -1,10 +1,9 @@
 package main.java.gamemanager;
 
+import main.java.additionalclasses.InputFileParser;
+import main.java.additionalclasses.Position;
 import main.java.enums.Move;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -12,16 +11,22 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import main.java.player.Player;
 import main.java.player.PlayerFactory;
+import java.io.File;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GameManagerTest {
 
     private GameManager gameManager;
     private BufferedWriter fileWriter = null;
+    private String mazeTestFilePath = "C:\\Users\\yl0871\\yuval\\maze.txt";
+    private File mazeFile = new File(mazeTestFilePath);
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -33,16 +38,16 @@ public class GameManagerTest {
     @Mock
     private PlayerFactory playerFactory;
 
-//    @Before
-//    public void setUp() throws IOException {
+    @Before
+    public void setUp() throws IOException {
 //        folder.create();
 //        File createdFile = folder.newFile("test.txt");
 //        fileWriter = new BufferedWriter(new FileWriter(createdFile));
-//
-//        when(playerFactory.createPlayer(any(), anyInt())).thenReturn(main.java.player);
-//
-//        gameManager = new GameManager(playerFactory);
-//    }
+
+        when(playerFactory.createPlayer(any(),anyInt())).thenReturn(player);
+
+        gameManager = new GameManager(playerFactory, InputFileParser.getMaze(mazeFile));
+    }
 
     @After
     public void teardown() throws IOException {
@@ -53,22 +58,76 @@ public class GameManagerTest {
         fileWriter = null;
     }
 
+
     @Test
-    public void happy(){
-        Mockito.when(player.move()).thenReturn(Move.LEFT);
-
-        boolean isSolved = gameManager.playGame();
-
-        Assert.assertTrue(isSolved);
+    public void moveLeftTest(){
+        gameManager.movePlayer(Move.LEFT);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(2,1);
+        Assert.assertEquals(playerPosition,expectedPosition);
     }
 
     @Test
-    public void fail(){
-        Mockito.when(player.move()).thenReturn(Move.LEFT).thenReturn(Move.RIGHT);
-
-        boolean isSolved = gameManager.playGame();
-
-        Assert.assertFalse(isSolved);
+    public void moveRightTest(){
+        gameManager.movePlayer(Move.RIGHT);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(2,3);
+        Assert.assertEquals(playerPosition,expectedPosition);
     }
+
+    @Test
+    public void moveUpTest(){
+        gameManager.movePlayer(Move.UP);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(1,2);
+        Assert.assertEquals(playerPosition,expectedPosition);
+    }
+
+    @Test
+    public void moveDownTest(){
+        gameManager.movePlayer(Move.DOWN);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(3,2);
+        Assert.assertEquals(playerPosition,expectedPosition);
+    }
+
+    @Test
+    public void moveLeftAndDownTest(){
+        gameManager.movePlayer(Move.DOWN);
+        gameManager.movePlayer(Move.LEFT);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(3,1);
+        Assert.assertEquals(playerPosition,expectedPosition);
+    }
+
+    @Test
+    public void hitWallTest(){
+        gameManager.movePlayer(Move.DOWN);
+        gameManager.movePlayer(Move.LEFT);
+        gameManager.movePlayer(Move.LEFT);
+        Mockito.doAnswer((answer) -> {return null;}).when(player).hitWall();
+        verify(player, Mockito.times(1)).hitWall();
+    }
+
+    @Test
+    public void moveRightToTheOtherSide(){
+        gameManager.movePlayer(Move.RIGHT);
+        gameManager.movePlayer(Move.RIGHT);
+        gameManager.movePlayer(Move.RIGHT);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(2,0);
+        Assert.assertEquals(playerPosition,expectedPosition);
+    }
+
+    @Test
+    public void moveDownToTheOtherSide(){
+        gameManager.movePlayer(Move.DOWN);
+        gameManager.movePlayer(Move.DOWN);
+        gameManager.movePlayer(Move.DOWN);
+        Position playerPosition = gameManager.getPlayerPosition();
+        Position expectedPosition = new Position(0,2);
+        Assert.assertEquals(playerPosition,expectedPosition);
+    }
+
 
 }
