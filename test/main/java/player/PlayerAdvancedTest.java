1ce4d6776867672d6e420b50451212bb4dac5a4b
Tal, Shahar (st9077) <st9077@intl.att.com>
2019-05-02 14:40:38 +0300

    Added another assert to hitwall test

--- test/main/java/player/PlayerAdvancedTest.java
+++ test/main/java/player/PlayerAdvancedTest.java
@@ -25,7 +25,7 @@ public class PlayerAdvancedTest {
         Assert.assertEquals("bookmark sequence should be 1", 1, player.getSeqNumber());
         Assert.assertTrue("bookmark 1 should be in map", player.getBookmarksMap().containsKey(1));
         Assert.assertEquals("bookmark 1 should contain last move as value", move, player.getBookmarksMap().get(1).get(0));
-
+        Assert.assertEquals("last move should be bookmark", Move.BOOKMARK, player.getLastMove());
     }
 
     @Test
