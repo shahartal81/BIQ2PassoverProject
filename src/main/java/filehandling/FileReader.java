fea207e77d09bf838f9f452eb3f73e3426920414
Shabanov, Marina (ms656r) <ms656r@intl.att.com>
2019-05-07 11:01:04 +0300

    Changes after merge

---
+++ src/main/java/filehandling/FileReader.java
@@ -0,0 +1,38 @@
+package main.java.filehandling;
+
+import main.java.gamemanager.GameLoader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    private GameLoader gameLoader = new GameLoader();
+
+    public List<String> readFromFile(File fileIn){
+        try(BufferedReader readFile = new BufferedReader(new java.io.FileReader(fileIn))){
+            return readFromFile(readFile);
+        }
+        catch (FileNotFoundException e){
+            gameLoader.addToErrorList("File not found. Exception: " + e);
+        }
+        catch (IOException e){
+            gameLoader.addToErrorList("Reading from file failed: " + e);
+        }
+        return null;
+    }
+
+    List<String> readFromFile(BufferedReader readFile) throws IOException {
+        List<String> result = new ArrayList<>();
+        String line;
+        while((line = readFile.readLine()) != null) {
+            if (line.trim().length() > 0) {
+                result.add(line);
+            }
+        }
+        return result;
+    }
+}
