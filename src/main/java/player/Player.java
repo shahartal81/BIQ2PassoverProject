f7a58865de87deb74c94bcf12b1f2b0449b1de63
Yuval Lev Naor <yl0871@intl.att.com>
2019-06-12 17:23:33 +0300

    Added a method to print games results + unit test

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -6,4 +6,8 @@ public interface Player {
     Move move();
     void hitWall();
     void hitBookmark(int seq);
+
+    default String getPlayerName() {
+        return getClass().getSimpleName();
+    }
 }
