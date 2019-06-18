a75c486a10b6487203e363061a5a36f8b10dc528
“Tl, Shahar (st9077)â <st9077@intl.att.com>
2019-06-18 13:28:38 +0300

    Added logic in factory for new player

--- src/main/java/player/PlayerFactory.java
+++ src/main/java/player/PlayerFactory.java
@@ -8,9 +8,10 @@ public class PlayerFactory {
     public Player createPlayer(Position mazeDimensions, Integer maxSteps){
         if ((mazeDimensions.getColumn() * mazeDimensions.getRow()) * 3 < maxSteps){
             return new PlayerBookmarkEachStep();
-        } else {
-//            return new PlayerRuleSet();
+        } else if (mazeDimensions.getColumn() < 3 && mazeDimensions.getRow() < 3){
             return new PlayerDistantGoing();
+        } else {
+            return new PlayerRuleSet();
         }
     }
 }
